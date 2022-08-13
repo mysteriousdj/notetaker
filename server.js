0f68b51c188bd2ef58a2719819f32cbe924a4dff
Dasol Jessi Lee <dasol.jlee@gmail.com>
2022-08-13 11:57:43 -0700

    function delete noted inputed

--- server.js
+++ server.js
@@ -46,6 +46,22 @@ function createNewNote(body, notesArray) {
     return newNote;
 }
 
+function deleteNote(id, notesArray) {
+    for (let i = 0; i < notesArray.length; i++) {
+        let note = notesArray[i];
+
+        if (note.id == id) {
+            notesArray.splice(i, 1);
+            fs.writeFileSync(
+                path.join(__dirname, './db/db.json'),
+                JSON.stringify(notesArray, null, 2)
+            );
+
+            break;
+        }
+    }
+}
+
 app.post('/api/notes', (req, res) => {
     const newNote = createNewNote(req.body, allNotes);
     res.json(newNote);
